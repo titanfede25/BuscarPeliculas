415947b3c9f075439dae0ee8821a2a00e921bfb5
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-04-26 12:14:18 -0300

    26/4

--- buscadorPeliculas/js.js
+++ buscadorPeliculas/js.js
@@ -1,23 +1,36 @@
 const obtener = (e) =>{
     e.preventDefault();
-    let tipo    = document.getElementById(tipo).value;
-    let anio    = document.getElementById(anio).value;
-    let texto   = document.getElementById(texto).value;
+    let tipo    = document.getElementById("tipo").value;
+    let anio    = document.getElementById("anio").value;
+    let texto   = document.getElementById("texto").value;
 
-    if (tipo.isNull()){
-        const fetch = fetch(`http://www.omdbapi.com/?apikey=4830b820&s=${texto}&y=${anio}`)
-    }
-    else if (anio.isNull()){
-        const fetch = fetch(`http://www.omdbapi.com/?apikey=4830b820&type=${tipo}&s=${texto}`)
-    }
-    else{
-        const fetch = fetch(`http://www.omdbapi.com/?apikey=4830b820&type=${tipo}&s=${texto}&y=${anio}`)
-    }
+    fetch(`http://www.omdbapi.com/?apikey=4830b820&type=${tipo}&s=${texto}&y=${anio}`)
     .then(res => res.json()) 
     .then(res => {
         console.log(res)
-        if (res.isArray()){
-            console.log("es array")
-        }   
-    })     
+        let campos = document.createElement("tr");
+        let campoNombre = document.createElement("th");
+        campoNombre.innerHTML = "Nombre";
+        let campoTipo = document.createElement("th");
+        campoTipo.innerHTML = "Tipo";
+        let campoAnio = document.createElement("th");
+        campoAnio.innerHTML = "Año";
+        campos.appendChild(campoNombre);
+        campos.appendChild(campoTipo);
+        campos.appendChild(campoAnio);
+        document.getElementById("table").appendChild(campos);
+        res.Search.forEach(unidad => {
+            let contenido = document.createElement("tr");
+            let contenidoNombre = document.createElement("td");
+            contenidoNombre.innerHTML = unidad.Title;
+            let contenidoTipo = document.createElement("td");
+            contenidoTipo.innerHTML = unidad.Type;
+            let contenidoAnio = document.createElement("td");
+            contenidoAnio.innerHTML = unidad.Year;
+            contenido.appendChild(contenidoNombre);
+            contenido.appendChild(contenidoTipo);
+            contenido.appendChild(contenidoAnio);
+            document.getElementById("table").appendChild(contenido);
+        })
+    })
 }
